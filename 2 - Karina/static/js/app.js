4d0e6dbbc56f0ea4022f4e20f6875f96b8f64349
Karina <53406090+kmunjal@users.noreply.github.com>
2020-02-04 23:16:34 -0800

    Add files via upload

---
+++ 2 - Karina/static/js/app.js
@@ -0,0 +1,41 @@
+function show_results(data){
+	var lr_str = '<h4>Logistic Regression: ' + data.logisticRegression +'</h4>'+'<br>'
+	var rf_str = '<h4>Random Forest: ' + data.randomForest +'</h4>'+'<br>'
+
+	document.getElementById("res_show").innerHTML=lr_str+rf_str;
+	document.getElementById("res_jumbo").style.display = "block";
+};
+
+function loadJsonData(hd, st, hr) {
+  // console.log("im in the loadData function!")
+  url = '/dynamic_results/'+hd+'/'+st+'/'+hr
+  console.log(url)
+
+  d3.json(url).then((data) => {
+
+    // console.log("im in the json data to do area!")
+    // console.log(data)
+    // console.log(data.logisticRegression)
+    // console.log(data.randomForest)
+    show_results(data);
+  });
+}
+
+$("#my_form").submit(function(e) {
+
+  e.preventDefault();
+  
+  var have_dog = document.getElementById('have_dog').value;
+  var sample_type = document.getElementById('sample_type').value;
+  var human_role = document.getElementById('human_role').value;
+  
+  // console.log(have_dog);
+  // console.log(sample_type);
+  // console.log(human_role);
+
+  loadJsonData(have_dog, sample_type, human_role)
+
+  console.log("done with ML")
+    
+});
+
