a47bde0ecc9250b2714facc4b0bb8be6c68261cf
Paul G <38454806+pGuillergan@users.noreply.github.com>
2020-02-06 12:01:12 -0800

    added something

---
+++ 0 - Final_test (TEST)/static/js/app4.js
@@ -0,0 +1,28 @@
+var trace1 = {
+    x: ['skin', 'saliva', 'stool'],
+    y: [561.04, 176.50, 360.23],
+    name: 'Has Dog',
+    type: 'bar'
+  };
+  
+  var trace2 = {
+    x: ['skin', 'saliva', 'stool'],
+    y: [453.19, 190.69, 336.64],
+    name: 'Does not have Dog',
+    type: 'bar'
+  };
+  
+  var data = [trace1, trace2];
+  
+  var layout = {
+      barmode: 'group',
+      xaxis: {
+        title: "Sample Type",
+      },
+      yaxis: {
+        title: "Microbial Diversity",
+      },
+      title: 'Human with Dog'
+    };
+  
+  Plotly.newPlot('myDiv_3', data, layout);
